55940630bcd049303ee06aa6729b42cb7d72da66
AdrianaIaffa <adrianaiaffa@hotmail.com>
2022-10-18 14:35:24 +0100

    add needed variables

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const qwerty = document.getElementById("qwerty");
+const phrase = document.getElementById("phrase");
+const btnReset = document.getElementsByClassName("btn__reset");
+const missed = 0;
