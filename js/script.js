9904e2281d864b57225c2423a282d36cc9bfe1f5
AdrianaIaffa <adrianaiaffa@hotmail.com>
2022-10-18 14:42:19 +0100

    Declare and initialize the phrases array

--- js/script.js
+++ js/script.js
@@ -2,3 +2,28 @@ const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
 const btnReset = document.getElementsByClassName("btn__reset");
 const missed = 0;
+
+
+const getRandomPhraseAsArray = arr => {
+
+}
+
+const addPhraseToDisplay = arr => {
+
+}
+
+const checkLetter = button => {
+
+}
+
+const checkWin = () => {
+
+}
+
+startButton.addEventListener("click", () => {
+
+});
+
+qwerty.addEventListener("click", e => {
+
+});
